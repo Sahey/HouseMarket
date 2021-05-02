4d1d68d664b8a39e851ee9035368f6e6e453977e
Sahey <s.ignatyev@corp.mail.ru>
2021-05-02 10:34:21 +0300

    add municipality view

--- HouseMarket/Sources/Modules/PropertyList/PropertyListViewController.swift
+++ HouseMarket/Sources/Modules/PropertyList/PropertyListViewController.swift
@@ -42,10 +42,11 @@ final class ColumnFlowLayout: UICollectionViewFlowLayout {
 }
 
 final class PropertyListViewController: UIViewController {
-    private var flowLayout: ColumnFlowLayout!
+    private var flowLayout: UICollectionViewFlowLayout!
     private var collectionView: UICollectionView!
 
     private typealias PropertyCell = CollectionCell<PropertyView>
+    private typealias MunicipalityCell = CollectionCell<MunicipalityView>
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,7 +55,16 @@ final class PropertyListViewController: UIViewController {
 
     private func setup() {
         view.backgroundColor = .design(.background(style: .primary))
-        flowLayout = ColumnFlowLayout(height: 180)
+        flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.sectionInset = UIEdgeInsets(
+            top: flowLayout.minimumInteritemSpacing,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
+        flowLayout.sectionInsetReference = .fromSafeArea
+
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
         collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         collectionView.backgroundColor = .design(.background(style: .primary))
@@ -65,17 +75,40 @@ final class PropertyListViewController: UIViewController {
         collectionView.delegate = self
 
         collectionView.register(PropertyCell.self, forCellWithReuseIdentifier: "PropertyCell")
+        collectionView.register(MunicipalityCell.self, forCellWithReuseIdentifier: "MunicipalityCell")
     }
 }
 
-extension PropertyListViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension PropertyListViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row % 3 == 0 {
+            return CGSize(
+                width: collectionView.bounds.inset(by: collectionView.layoutMargins).size.width,
+                height: 230
+            )
+        } else {
+            return CGSize(
+                width: collectionView.bounds.inset(by: collectionView.layoutMargins).size.width,
+                height: 180
+            )
+        }
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PropertyCell", for: indexPath)
-        if let propertyCell = cell as? PropertyCell {
+        let cell: UICollectionViewCell
+
+        if indexPath.row % 3 == 0 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MunicipalityCell", for: indexPath)
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PropertyCell", for: indexPath)
+        }
+
+        switch cell {
+        case let propertyCell as PropertyCell:
             propertyCell.view.configure(
                 viewModel: PropertyViewModel(
                     id: 1,
@@ -89,7 +122,21 @@ extension PropertyListViewController: UICollectionViewDataSource, UICollectionVi
                     daysPosted: "1 dagar"
                 )
             )
+        case let municipalityCell as MunicipalityCell:
+            municipalityCell.view.configure(
+                viewModel: MunicipalityViewModel(
+                    id: 1,
+                    title: "Omrade",
+                    city: "Stockholm",
+                    rating: "Betyg: 4.5/5",
+                    imageUrl: nil,
+                    averagePrice: "Snittpris: 20 014 kr/m"
+                )
+            )
+        default:
+            fatalError("Unexpected cell type")
         }
+
         return cell
     }
 }
---
+++ HouseMarket/Sources/Views/MunicipalityView.swift
@@ -0,0 +1,94 @@
+//
+//  MunicipalityView.swift
+//  HouseMarket
+//
+//  Created by Sahey Ignatyev on 02.05.2021.
+//
+
+import MapKit
+import UIKit
+
+struct MunicipalityViewModel {
+    let id: Int
+
+    let title: String
+    let city: String
+    let rating: String
+    let imageUrl: String?
+    let averagePrice: String
+}
+
+final class MunicipalityView: UIView {
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = .design(.header(style: .h1))
+        titleLabel.textColor = .design(.text(style: .primary))
+        return titleLabel
+    }()
+
+    private let areaImageView: UIImageView = {
+        let areaImageView = UIImageView()
+        areaImageView.backgroundColor = .gray
+        areaImageView.contentMode = .scaleAspectFill
+        areaImageView.layer.cornerRadius = 6
+        return areaImageView
+    }()
+
+    private let cityLabel: UILabel = {
+        let cityLabel = UILabel()
+        cityLabel.font = .design(.header(style: .h2))
+        cityLabel.textColor = .design(.text(style: .primary))
+        return cityLabel
+    }()
+
+    private let ratingLabel: UILabel = {
+        let ratingLabel = UILabel()
+        ratingLabel.font = .design(.paragraph(style: .p1))
+        ratingLabel.textColor = .design(.text(style: .primary))
+        return ratingLabel
+    }()
+
+    private let averagePriceLabel: UILabel = {
+        let averagePriceLabel = UILabel()
+        averagePriceLabel.font = .design(.paragraph(style: .p1))
+        averagePriceLabel.textColor = .design(.text(style: .primary))
+        return averagePriceLabel
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+        let stackView = UIStackView(arrangedSubviews: [
+            titleLabel,
+            areaImageView,
+            cityLabel,
+            ratingLabel,
+            averagePriceLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    func configure(viewModel: MunicipalityViewModel) {
+        titleLabel.text = viewModel.title
+        cityLabel.text = viewModel.city
+        ratingLabel.text = viewModel.rating
+        averagePriceLabel.text = viewModel.averagePrice
+    }
+}
